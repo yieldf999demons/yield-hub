cd214a0a9d788566292e8386858e2c18cefd46e1
yieldf999demons <bobbyhawkin969@gmail.com>
2026-02-09 17:49:55 -0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,1 @@
-print("LOADER START")
-
-local url = "https://raw.githubusercontent.com/yieldf999demons/yield-hub/main/v1.lua"
-local src = game:HttpGet(url)
-
-print("FETCHED SCRIPT")
-loadstring(src)()
+local BASE_URL = "https://raw.githubusercontent.com/yieldf999demons/yield-hub/main/"
